6a1f8a77848724aafc89209b17889ab09fb962dd
Ethan Collins <ethantcollins98@gmail.com>
2019-07-22 16:51:15 -0600

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,17 @@
+require 'pry'
+
 require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
+doc = Nokogiri::HTML(html)
 
+courses = doc.css(".tout__label.heading.heading--level-4")
 
+p courses[0].attributes
+# courses.each do |course|
+#     puts course.text.strip
+# end
+
+#binding.pry
